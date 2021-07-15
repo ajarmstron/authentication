546fa3d66a8b7a1c81a6a31842ce8f87573233e9
Alec Armstrong <ajarmstron@aol.com>
2021-07-16 00:04:42 +0100

    Level 5 - adding cookes and sessions

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
+const session = require('express-session');
+const passport=require("passport");
+const passportLocalMongoose=require("passport-local-mongoose");
 
 const app = express();
+
 
 //console.log(process.env.API_KEY);
 
@@ -17,18 +19,37 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+app.use(session({
+  secret: 'Our little secret.',//this will be in the environment file
+  resave: false,
+  saveUninitialized: true,
+  cookie: {}
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+mongoose.set("useCreateIndex", true);
 
 const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
 
+userSchema.plugin(passportLocalMongoose);
 
-// var secret = process.env.SOME_LONG_UNGUESSABLE_STRING;
-// userSchema.plugin(encrypt, { secret: secret });
+
 
 const User = new mongoose.model("User", userSchema);
+
+// CHANGE: USE "createStrategy" INSTEAD OF "authenticate"
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 app.get("/", function(req,res){
   res.render("home");
@@ -42,45 +63,48 @@ app.get("/register", function(req,res){
   res.render("register");
 });
 
+app.get("/secrets", function(req, res){
+    if (req.isAuthenticated()){
+      res.render("secrets");
+    } else {
+      res.redirect("/login");
+    }
+});
+
+app.get("/logout", function(req, res){
+  req.logout();
+  res.redirect("/");
+});
+
 app.post("/register", function(req, res){
-
-  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-      // Store hash in your password DB.
-      const newUser = new User({
-        email: req.body.username,
-        password: hash
+    User.register({username:req.body.username}, req.body.password, function(err, user) {
+    if (err) {
+        console.log(err);
+        res.redirect("/register");
+      } else {
+        passport.authenticate("local")(req, res, function(){
+        res.redirect("/secrets");
       });
+    }
 
-        newUser.save(function(err){
-          if (err) {
-            console.log(err);
-          } else {
-          res.render("secrets");
-          }
-        });
+      // Value 'result' is set to false. The user could not be authenticated since the user is not active
+    });
   });
 
-});
-
 app.post("/login", function(req, res){
-  const username = req.body.username;
-  const password = req.body.password;
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
 
-  User.findOne({email: username}, function(err, foundUser) {
-      if (err) {
-        console.log(err);
-      } else {
-        if (foundUser) {
-          bcrypt.compare(password, foundUser.password, function(err, result) {
-              // result == true
-              if (result===true){
-                res.render("secrets");
-              }
-          });
-
-        }
-      }
-});
+  req.login(user, function(err){
+    if (err) {console.log(err);
+  } else {
+    passport.authenticate("local")(req, res, function(){
+    res.redirect("/secrets");
+    });
+    }
+  });
 });
 
 app.listen(3000, function() {
